238b46e83e5e44e22ccc3d17eb3baac8ba3dfecb
Andrii Ivanov <ipzk241_iao@student.ztu.edu.ua>
2026-02-28 14:15:54 +0200

    Add GDPR cookie consent popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import CookieConsent from "react-cookie-consent";
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
@@ -12,6 +13,22 @@ function App() {
         <Route path="/game" element={<GamePage />} />
         <Route path="/leaderboard" element={<LeaderboardPage />} /> {/* Маршрут */}
       </Routes>
+
+      {/* GDPR Банер */}
+      <CookieConsent
+        location="bottom"
+        buttonText="Зрозуміло, погоджуюсь"
+        declineButtonText="Відмовитись"
+        enableDeclineButton
+        cookieName="memoryCardsGDPRConsent"
+        style={{ background: "#2B373B", alignItems: "center" }}
+        buttonStyle={{ background: "#4e503b", color: "white", fontSize: "14px", borderRadius: "5px", padding: "8px 15px" }}
+        declineButtonStyle={{ fontSize: "14px", borderRadius: "5px", padding: "8px 15px", background: "transparent", border: "1px solid white", color: "white" }}
+        expires={150}
+      >
+        Цей веб-сайт використовує файли cookie для покращення користувацького досвіду та збору базової аналітики відповідно до GDPR.{" "}
+        <span style={{ fontSize: "12px", display: "block", marginTop: "5px" }}>Продовжуючи гру, ви погоджуєтеся з нашою політикою.</span>
+      </CookieConsent>
     </div>
   );
 }
